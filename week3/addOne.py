4184bdf12559945fb8bcadb3c493646a1ada8de7
Stuart Saint <stuartsaint242@gmail.com>
2022-01-31 20:25:16 +0000

    Addition: addOne script

---
+++ week3/addOne.py
@@ -0,0 +1,8 @@
+# Reads a number and adds 1 to it
+
+# int converts a string to an integer
+yourNumber = int(input("Please enter a number: "))
+
+newNumber = yourNumber + 1
+
+print("{} + 1 = {}".format(yourNumber, newNumber))
--- week3/readName.py
+++ week3/readName.py
@@ -1,4 +1,9 @@
 # takes an input and prints it to the terminal
 
 name = input('Enter your name: ')
+
 print("Hello {}!".format(name))
+
+age = input("What is your age?: ")
+
+print("Your age is {}.".format(age))
