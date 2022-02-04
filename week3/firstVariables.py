4cd39b6d434d835d98d5c74d042cdaa0ef687d59
Stuart Saint <stuartsaint242@gmail.com>
2022-02-04 19:54:44 +0000

    Renaming of weekly files

---
+++ week3/firstVariables.py
@@ -0,0 +1,9 @@
+# introduction to and testing of variables
+age = '34'
+nextYear = int(age) + 1
+
+print(type(age))
+
+# conversion of variable types
+print("You are {}.".format(age))
+print("Next year you will be " + str(nextYear))
