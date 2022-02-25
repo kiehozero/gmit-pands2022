83a30ce255f5d9f46460dd673d8aa64e1aaf47fb
Stuart Saint <stuartsaint242@gmail.com>
2022-02-25 18:36:18 +0000

    Week 5 and 6 walkthroughs

---
+++ week5/messingWithDicts.py
@@ -0,0 +1,25 @@
+make = "ford"
+model = "mondeo"
+year = 2006
+owner = "andrew"
+
+# causes issues once you want to store more than one vehicle
+
+car = {
+    "make" : "ford" ,
+    "model" : "mondeo" ,
+    "year" : 2006 ,
+    "owner" : {
+        "name" : "andrew" ,
+        "number" : "1123"
+    }
+}
+
+print ( car [ "owner" ] [ "name" ] )
+# get will return a value if the key is present, but won't throw an error if it isn't present
+print ( car.get ( "registration" ) )
+# keys returns the keys in the dictionary as a List, values does the same for the second part of the pair
+print ( car.keys ( ) )
+print ( car [ "owner" ].keys ( ) )
+# items will return each key-value pair as a Tuple within a List
+print ( car.items ( ) )
