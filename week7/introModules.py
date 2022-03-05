0f91c2f46527c4e35ff9ff5d8647dcbbe9031c25
Stuart Saint <stuartsaint242@gmail.com>
2022-03-05 14:57:42 +0000

    File and content creation

---
+++ week7/introModules.py
@@ -0,0 +1,8 @@
+# import math
+# above brings in the whole library, below just the function you requre
+from math import cos
+# import math as m
+# above allows you to redefine the library for convenience
+var = math.cos ( 90 )
+
+print ( var )
