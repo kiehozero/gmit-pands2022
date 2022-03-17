cd2c36c9bc555a65c3ec2410c6c5b76dd78a43d5
Stuart Saint <stuartsaint242@gmail.com>
2022-03-17 19:49:00 +0000

    Addition: array and dimension information

--- week8/introNumPy.py
+++ week8/introNumPy.py
@@ -17,6 +17,12 @@ numbers = numbers * np.array([1,4,1,4,1,4,1])
 
 print(numbers)
 
+print(type(numbers))
+
+# you can add array elements together
+sum = numbers[2] + numbers[3]
+print(f"{numbers[2]} plus {numbers[3]} equals {sum}")
+
 randomNum = np.random.randint(100,200,10)
 
 print(randomNum)
@@ -24,3 +30,24 @@ print(randomNum)
 normDistro = np.random.normal(100,1,10)
 
 print(normDistro)
+
+conv = np.array([["type",1,2,3],["mph",10,20,30],["kph", 20,30,50]])
+
+print(conv)
+
+# ndim returns the number of dimensions an array has
+dimCount = conv.ndim
+
+print(f"Conv has {dimCount} dimensions")
+
+# you can preset the number of dimensions required using ndmin, I guess
+# this is to allow for stuffing of blank dimensions at a later stage?
+arr = np.array([["a", "b", "c", "d"], [1,2,3,4]], ndmin=4)
+dims = arr.ndim
+
+print(arr)
+print(f"Arr has {dims} dimensions")
+
+# you can access array elements using comma-separated integers
+# note the zeroes below are because of the additional dimensions specified by ndmin
+print(f"The third element on the first row of the fourth dimension is {arr[0,0,1,3]}")
