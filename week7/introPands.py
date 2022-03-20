032b530fc33b0a583f2fc81351dce5141500f04d
Stuart Saint <stuartsaint242@gmail.com>
2022-03-20 21:33:53 +0000

    Addition: loc info

--- week7/introPands.py
+++ week7/introPands.py
@@ -11,6 +11,11 @@ myvar = pd.DataFrame(mydataset)
 
 print(myvar)
 
+# loc prints the returns each key and value from a specified row or rows. The
+# second of these will produce an additional dataframe to house the rows.
+print(myvar.loc[1])
+print(myvar.loc[[0,1]])
+
 # Series a one-dimensional array, but you can assign it labels using the index parameter
 
 a = [1, 7, 2]
@@ -20,6 +25,9 @@ b = ["thou", "hund", "tens"]
 newVar = pd.Series(a, index=b)
 
 print("tens".format(newVar["tens"]))
+
+# you can use loc again to search for an index
+print(newVar.loc["hund"])
 
 # By using a dictionary, you can use key/values as indices
 
