8086fe2272d38206530b5f6c7e0aa6948a881527
Stuart Saint <stuartsaint242@gmail.com>
2022-04-14 21:11:46 +0100

    Addition: final pandas tutorial

--- week11/analysegrades.py
+++ week11/analysegrades.py
@@ -4,11 +4,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# I made this var to make it easier to change the location of the files
-path = '../data/'
-filenameForGrades = path + 'grades.csv'
+filenameForGrades = 'grades.csv'
 
-# there is an index col in this csv
+# there is an index col in this csv, this is not mandatory but will be provided automatically by some data feeds
 df = pd.read_csv(filenameForGrades, index_col=0)
 
 # get rid of nulls and duplicates
@@ -21,22 +19,21 @@ df.drop_duplicates(inplace=True)
 df = df.pivot_table(values='grade',index=['name','subject'],aggfunc='max').reset_index()
 print(df)
 
-# group by a value in a column
+# group by a value in a column. Mean is a standard statistical function in python, you can also use min, max, etc.
 meanValues = df.groupby('name').mean()
 print(meanValues)
 
+# re-structures the data into the format required to properly populate the graph
 df = df.pivot(index='name',columns='subject',values='grade')
 
-# print a corralation table 
+# print a correlation table 
 print(df.corr())
 
 
 # plot a bar graph
 df.plot.bar()
 
-# I meant to say in the lecture if you want to plot a sub-set of the dataframe
-# then just select the parts you want to plot
-# eg
+# if you want to plot a sub-set of the dataframe then just select the parts you want to plot, e.g.:
 # df[['math', 'SIEM']].plot.bar()
 
 plt.show()
